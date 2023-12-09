ec5a17df7bed995447a84727346cb75377546f83
Oni <qw28425382694@gmail.com>
2023-12-09 23:00:28 +0800

    Ch5 - section110 數字運算

---
+++ Chapter5/110_數字運算/app.js
@@ -0,0 +1,13 @@
+/*     不要在意             */
+let x = 5;
+let y = 2;
+console.log(y ** x);
+
+let z = 1;
+a = z++;
+b = ++z;
+console.log(z);
+/* 猜猜看 數字是多少! */
+console.log(a, b);
+console.log(z++);
+console.log(z);
