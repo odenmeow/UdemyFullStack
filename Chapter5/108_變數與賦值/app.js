3014a10f281168a1eb7d07a0cd212f39956c9ed6
Oni <qw28425382694@gmail.com>
2023-12-09 22:27:34 +0800

    Ch5 - section108

---
+++ Chapter5/108_變數與賦值/app.js
@@ -0,0 +1,17 @@
+let x;
+x = 10;
+
+console.log(x);
+// 雖然可以不使用宣告關鍵字 但不建議忽略 let var const!
+// a = 5;
+
+// const 一定要初始值!
+// const y;
+
+const y = 10;
+y = 20;
+
+/*     24:00       突然回到 語法規則@@              */
+
+let _a = 5;
+let $a = 5;
