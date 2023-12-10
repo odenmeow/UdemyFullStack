4a5a4924ec21861934b21b046ed241f82c4317c4
Oni <qw28425382694@gmail.com>
2023-12-11 00:29:05 +0800

    Ch5 - section119 truthy falsy value講解哪些放到if statement之類的地方 會被視為true 、1、0、false 這些。

---
+++ Chapter5/119_Truthy_and_Falsy_Values/app.js
@@ -0,0 +1,15 @@
+/*          Boolean Context           */
+/*                      */
+if ("天氣好") {
+  // true  !?
+  console.log("嗎");
+}
+
+console.log("" && "");
+
+let x = {};
+if (x) {
+  console.log("im true");
+} else {
+  console.log("im false");
+}
