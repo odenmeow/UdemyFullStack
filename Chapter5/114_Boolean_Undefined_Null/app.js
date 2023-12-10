48494cacc8f6bf1185d3a45bf432c767b7bbb45c
Oni <qw28425382694@gmail.com>
2023-12-10 17:13:23 +0800

    Ch5 - section114 boolean null undefined，函數預設回傳值是undefined，==比較的時候JAVA跟JS 有些不同之處我寫在筆記上。

---
+++ Chapter5/114_Boolean_Undefined_Null/app.js
@@ -0,0 +1,16 @@
+/*          Boolean, Undefined, Null            */
+/*         Boolean             */
+let isPremium = false;
+console.log(isPremium);
+console.log(!isPremium);
+console.log(isPremium == "false"); //false
+console.log(isPremium == "0"); // true
+console.log(!isPremium == "1"); // true
+console.log(!isPremium == "2"); // false
+/*         Undefined Null             */
+
+let x;
+let y;
+y = null;
+console.log("x=" + x); //undefined
+console.log("y=" + y); //null
