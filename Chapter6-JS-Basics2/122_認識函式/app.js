68eb5a259d91649d8df7f1de5a94c43237809734
Oni <qw28425382694@gmail.com>
2023-12-11 10:59:13 +0800

    Ch6 - section122 認識函數~跟JAVA些許不同之處、幫資料夾命名好一些不要只有CH2345

---
+++ Chapter6-JS-Basics2/122_認識函式/app.js
@@ -0,0 +1,16 @@
+/*       認識函數            */
+
+function f() {
+  console.log("第一個函數");
+}
+
+f(); // 寫了才會有輸出!
+
+/*      第二個帶參數   */
+function sayHi(name, meal) {
+  console.log("你好我" + name);
+  console.log("我要去吃" + meal + "了");
+}
+
+sayHi("oni");
+sayHi("oni", "午餐");
