e6ab0ecc4386117ef20c1c6a71fb236f365af83b
Oni <qw28425382694@gmail.com>
2023-12-11 10:25:34 +0800

    Ch5 - section121 慣例與限制

---
+++ Chapter5/121_CodingConventionAndRestrictions/app.js
@@ -0,0 +1,8 @@
+/*          LogicalOP           */
+/*         Coding Convention And Restrictions             */
+let hasPremium = true;
+let has_premium = true;
+
+console.log(1 + 2);
+
+const PI = 3.14;
