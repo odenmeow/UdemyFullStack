895f26a5154048b0f7a4b6848da47181b824aa11
Oni <qw28425382694@gmail.com>
2023-12-11 10:16:58 +0800

    Ch5 - section120 truthy falsy搭配 & | 的使用，應該如何有效做判斷。

---
+++ Chapter5/120_LogicalOp與其他資料類型處裡/app.js
@@ -0,0 +1,7 @@
+/*          LogicalOP           */
+/*                      */
+console.log(NaN || 1);
+let data = "";
+console.log(data || "資料讀取失敗"); // 得到  資料讀取失敗
+data = "oni";
+console.log(data || "資料讀取失敗"); //得到  oni
