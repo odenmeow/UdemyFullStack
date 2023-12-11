468db2423032df1c19cda590063c617acf744f21
Oni <qw28425382694@gmail.com>
2023-12-11 18:20:02 +0800

    Ch6 - section134 ,133略,134 Math介紹而已

---
+++ Chapter6-JS-Basics2/134_MathObject/app.js
@@ -0,0 +1,10 @@
+/*       Math         */
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.pow(3, 2));
+console.log(Math.random());
+console.log(Math.sqrt(2)); //1.414
+console.log(Math.abs(-11)); //11
+console.log(Math.floor(1.9991)); //1
+console.log(Math.ceil(1.00001)); //2
