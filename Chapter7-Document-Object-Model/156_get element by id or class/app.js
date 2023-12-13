72a1dd2ac694278f0ff41be647a1f1fba72f7b81
Oni <qw28425382694@gmail.com>
2023-12-13 22:09:36 +0800

    Ch7 - section156 getElement by id class...、DOM取得的HTML Collection、後續再介紹querySelector

---
+++ Chapter7-Document-Object-Model/156_get element by id or class/app.js
@@ -0,0 +1,9 @@
+/*             window             */
+let myH1 = document.getElementById("myH1");
+console.log(myH1);
+console.log(document.getElementById("myH1"));
+
+/*             getElementsByClassName             */
+
+let myparagraphs = document.getElementsByClassName("my-p");
+console.log(myparagraphs);
