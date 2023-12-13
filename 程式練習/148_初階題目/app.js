89672817c96e6f2f1137ab1c03b948651d72ff8f
Oni <qw28425382694@gmail.com>
2023-12-13 15:54:51 +0800

    程式練習 - section150 初階題目4-5 (中階題目後續回頭再用分支玩merge，目前先創造一個不那麼漂亮的commitBranch

--- 程式練習/148_初階題目/app.js
+++ 程式練習/148_初階題目/app.js
@@ -60,3 +60,95 @@ PPDI();
 // 370
 // 371
 // 407
+
+console.log("-----------4. calc_byManual---------");
+/*求s=a+aa+aaa+aaaa+aa...a的值，其中a是小於10的數字，相加的項數為n。
+例如，當a = 2，n=5時，s=2+22+222+2222+22222。若a  =2, n = 3，
+則輸出結果的形式如：2+22+222 = 246。
+編寫一個名為calc的函式，參數為a與n，回傳值為s。*/
+function calc_byFunction(units, terms) {
+  let arr = [];
+  for (let i = 0; i < terms; i++) {
+    let numberText = "";
+    for (let j = 0; j <= i; j++) {
+      numberText += Number(units);
+    }
+    arr.push(numberText);
+  }
+  console.log(arr);
+  let summary = 0;
+  do {
+    summary += Number(arr.pop());
+  } while (arr.length > 0);
+  console.log(summary);
+}
+function calc_byManual(units, terms) {
+  let sum = 0;
+  for (let i = 0; i < terms; i++) {
+    let number = units / 10; //第一項=*1
+    for (let j = 0; j <= i; j++) {
+      number = number * 10;
+      sum += number;
+    }
+  }
+  console.log(sum);
+}
+
+calc_byManual(2, 3); // returns 246
+calc_byManual(8, 5); // returns 98760
+console.log("-----------4. calc_byFunction---------");
+calc_byFunction(2, 3); // returns 246
+calc_byFunction(8, 5); // returns 98760
+
+console.log("-----------5. shuffle---------");
+/*1. 編寫一個名為"shuffle()”的函數，唯一的參數為一個array of integers，
+return type也是array of integers，返回的array與參數array的元素相同，
+但元素順序為隨機排序的結果。
+*/
+
+function shuffle(arr) {
+  //[0,1) *4 就會 0,1,2,3     *10  0,~9
+
+  let ranNum;
+  let randArr = [];
+  let times = arr.length;
+  while (arr.length) {
+    ranNum = Math.floor(Math.random() * arr.length);
+    randArr.push(arr[ranNum]);
+    arr.splice(ranNum, 1);
+  }
+
+  console.log(randArr);
+}
+
+shuffle([2, 11, 37, 42]);
+// returns 一個經過洗牌後的 array。
+// 可能是[42, 11, 37, 2]
+
+console.log("_----splice");
+const months = ["Jan", "March", "April", "June"];
+months.splice(1, 1);
+console.log(months);
+
+console.log("-------------shuffle SWAP----------------");
+
+function shuffle_Swap(arr) {
+  let currentIndex = arr.length;
+  let temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  // 好像做到>0就可以，因為最後一個自己跟自己交換
+  while (currentIndex !== 0) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // Swap it with the current element.
+    temporaryValue = arr[currentIndex];
+    arr[currentIndex] = arr[randomIndex];
+    arr[randomIndex] = temporaryValue;
+  }
+
+  return console.log(arr);
+}
+shuffle_Swap([2, 11, 37, 42]);
