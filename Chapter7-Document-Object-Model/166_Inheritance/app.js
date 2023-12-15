943919b8a137cc95579208eba0e9a4d6a6fc8472
Oni <qw28425382694@gmail.com>
2023-12-15 19:31:42 +0800

    Ch7 - section166 繼承，但只講觀念，唯一js寫了表單清空怎麼做而已

---
+++ Chapter7-Document-Object-Model/166_Inheritance/app.js
@@ -0,0 +1,8 @@
+/*                       Inheritance                     */
+/*           Inheritance          */
+console.log("----------Inheritance--------------");
+let btn = document.querySelector("button");
+btn.addEventListener("click", () => {
+  let form = document.querySelector("form");
+  form.reset();
+});
