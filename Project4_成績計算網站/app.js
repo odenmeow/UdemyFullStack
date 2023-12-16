38e2f2d9205175f4efc26b455ba5de006b73361e
Oni <qw28425382694@gmail.com>
2023-12-17 00:45:23 +0800

    Project4 - section173 開場動畫，接觸gsap、圖片object-Fit屬性、div使用background:linear-gradient功能，JS使用cdn.js提供的物件、快速做動畫，setTimeout、elementObject.style.pointerEvents='none' 可以使滑鼠事件穿透擋在前面的對象，這跟sec167 有點像呢?

---
+++ Project4_成績計算網站/app.js
@@ -0,0 +1,31 @@
+// let hero = document.querySelector(".hero");
+// let slider = document.querySelector(".slider");
+// let animation = document.querySelector("section.animation-wrapper");
+
+// const time_line = new TimelineMax();
+
+// //param 1是要控制的對象
+// //param 2是duration
+// //param 3是控制對象的原始狀態
+// //param 4是控制對象的動畫結束後狀態
+// //param 5提早進場
+// time_line
+//   .fromTo(
+//     hero,
+//     1,
+//     { height: "0%" },
+//     { height: "100%", ease: Power2.easeInOut } // 套件提供的功能
+//   )
+//   .fromTo(hero, 0.8, { width: "80%" }, { width: "100%" })
+//   .fromTo(
+//     slider,
+//     1,
+//     { x: "-100%" },
+//     { x: "0%", ease: Power2.easeInOut },
+//     "-=0.8"
+//   )
+//   .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
+
+// window.setTimeout(() => {
+//   animation.style.pointerEvents = "none";
+// }, 2100);
