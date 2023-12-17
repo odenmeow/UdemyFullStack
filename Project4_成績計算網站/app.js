f6c9a74b34d5930d85dd420a6243b8d36849b266
Oni <qw28425382694@gmail.com>
2023-12-17 20:41:17 +0800

    Project4 - section174 改變select顏色，使用了之前所學，JS、preventDefault防止enter表單送出、防止點擊垃圾桶送出、探測select標籤change的時候、以及target value、改變選單顏色之類【我偷回過去改變數名稱allbtns -> allBtns】

--- Project4_成績計算網站/app.js
+++ Project4_成績計算網站/app.js
@@ -29,3 +29,74 @@
 // window.setTimeout(() => {
 //   animation.style.pointerEvents = "none";
 // }, 2100);
+
+/*                  以上是動畫開場                                 */
+// 避免enter直接送出表單
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+// 防止form內部垃圾桶交出表單 (也防止所有默認按鈕行為)
+let allBtns = document.querySelectorAll("button");
+// 如果是HTMLCollection則不能forEach ， 除非透過Array.from(丟進來).forEach才能用
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+// 改變  選擇成績 選項opt對應顏色
+let allSelects = document.querySelectorAll("select");
+// console.log(allSelects);
+// allSelects.forEach((select) => {
+//   console.log(select);
+//   console.log(select.options);
+//   console.log(select.options.length);
+// });
+// 以上註解是我自己練習用
+
+allSelects.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    // console.log(e.target);
+    // console.log(e.target.value);
+    setGPA();
+    changeColor(e.target);
+  });
+});
+
+function changeColor(target) {
+  if (target.value == "A" || target.value == "A-") {
+    target.style.backgroundColor = "lightgreen";
+    target.style.color = "black";
+  } else if (
+    target.value == "B" ||
+    target.value == "B-" ||
+    target.value == "B+"
+  ) {
+    target.style.backgroundColor = "yellow";
+    target.style.color = "black";
+  } else if (
+    target.value == "C" ||
+    target.value == "C-" ||
+    target.value == "C+"
+  ) {
+    target.style.backgroundColor = "orange";
+    target.style.color = "black";
+  } else if (
+    target.value == "D" ||
+    target.value == "D-" ||
+    target.value == "D+"
+  ) {
+    target.style.backgroundColor = "red";
+    target.style.color = "white";
+  } else if (target.value == "F") {
+    target.style.backgroundColor = "gray";
+    target.style.color = "white";
+  } else {
+    target.style.backgroundColor = "gray";
+    target.style.color = "white";
+  }
+}
+function setGPA() {}
