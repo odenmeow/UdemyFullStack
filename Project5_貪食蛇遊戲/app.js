2490290ddf543dd02e076b0a685b76650108d4c8
Oni <qw28425382694@gmail.com>
2023-12-22 23:27:29 +0800

    Project5 - section204 移動蛇，使用pop、unshift 還有搭配setInterval 可以達成移動的效果，然後要記得重新畫背景，因為畫過的東西會殘留，使用ctx.fillStyle +fillRect 讓整個背景每次draw都先還原黑色

--- Project5_貪食蛇遊戲/app.js
+++ Project5_貪食蛇遊戲/app.js
@@ -30,13 +30,41 @@ snake[3] = {
   y: 0,
 };
 
-for (let i = 0; i < snake.length; i++) {
-  if (i) {
-    ctx.fillStyle = "lightblue";
-  } else {
-    ctx.fillStyle = "lightgreen";
+let d = "Right";
+
+function draw() {
+  console.log("drawing");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < snake.length; i++) {
+    if (i) {
+      ctx.fillStyle = "lightblue";
+    } else {
+      ctx.fillStyle = "lightgreen";
+    }
+    ctx.strokeStyle = "white";
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
   }
-  ctx.strokeStyle = "white";
-  ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
-  ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
+
+  // 以目前的d 變數方向決定蛇的下一影格
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+  if (d == "Left") {
+    snakeX -= unit;
+  } else if (d == "Right") {
+    snakeX += unit;
+  } else if (d == "Up") {
+    snakeY -= unit;
+  } else if (d == "Down") {
+    snakeY += unit;
+  }
+  let newHead = {
+    x: snakeX,
+    y: snakeY,
+  };
+  // 如果吃到果實 unshift就好   沒吃到就 pop+unshift
+  snake.pop();
+  snake.unshift(newHead);
 }
+let myGame = setInterval(draw, 100);
