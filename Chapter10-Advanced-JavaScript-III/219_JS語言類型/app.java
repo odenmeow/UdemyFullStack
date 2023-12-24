ea8c31a25ebcc3362d9b7c45d7529b80c802ddc6
Oni <qw28425382694@gmail.com>
2023-12-24 16:20:42 +0800

    Ch10 - section219  JS語言類型 沒寫code

---
+++ Chapter10-Advanced-JavaScript-III/219_JS語言類型/app.java
@@ -0,0 +1,14 @@
+
+public class app {
+    public static void main(String... args){
+        int a=5;
+        String b="AAA";
+        String c=a+b;
+        // Can't place chinese here , only ascii  english.
+        double d=333.212f;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println((int)d);
+        
+    }
+}
