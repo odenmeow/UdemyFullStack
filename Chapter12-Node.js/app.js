19e71bf4b8e31f7326b9ccb93c59cc2143000bf7
Oni <qw28425382694@gmail.com>
2023-12-26 12:43:24 +0800

    Ch12 - section246 npm、nodemon，後者可替代node app.js 然後每次重新儲存都自動幫忙重新執行，不需要人工停止再啟動、前面則說明如何安裝、如何使用、package.json 管理套件、npm install -g 則是讓全電腦都可以使用，日後不必每個專案都重新安裝 nodemon 這種東西、還有玩cowsay

--- Chapter12-Node.js/app.js
+++ Chapter12-Node.js/app.js
@@ -1,47 +1,9 @@
-const http = require("http");
-const fs = require("fs");
-let myfilePath = "C" + __dirname.substring(1); //0不需要，從1開始擷取
+var cowsay = require("cowsay");
 
-// request obj  ,  response obj
-const server = http.createServer((req, res) => {
-  //   console.log(req.headers);
-  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-  //   res.write("歡迎來到我的網頁");
-  //   res.end();
-  //   console.log(req.url); // 請求的url是什麼
-
-  if (req.url == "/") {
-    res.write("歡迎來到我的網頁");
-    res.end();
-    // 如果沒有這個會導致轉圈圈的問題，瀏覽器收不到回應完成的訊號
-    // 必須一個一個增加，因為asynchronous 會導致可能先end() 結果還沒送出檔案
-  } else if (req.url == "/anotherPage") {
-    res.write("歡迎來到另一個頁面");
-    res.end();
-    // 如果沒有這個會導致轉圈圈的問題，瀏覽器收不到回應完成的訊號
-    // 必須一個一個增加，因為asynchronous 會導致可能先end() 結果還沒送出檔案
-  } else if (req.url == "/myFile") {
-    fs.readFile(myfilePath + "/myFile.html", (e, data) => {
-      if (e) {
-        res.write("存取html文件出錯");
-        res.end();
-        // 如果沒有這個會導致轉圈圈的問題，瀏覽器收不到回應完成的訊號
-        // 必須一個一個增加，因為asynchronous 會導致可能先end() 結果還沒送出檔案
-      } else {
-        res.write(data);
-        res.end();
-        // 如果沒有這個會導致轉圈圈的問題，瀏覽器收不到回應完成的訊號
-        // 必須一個一個增加，因為asynchronous 會導致可能先end() 結果還沒送出檔案
-      }
-    });
-  } else {
-    res.write("不存在這頁面");
-    res.end();
-    // 如果沒有這個會導致轉圈圈的問題，瀏覽器收不到回應完成的訊號
-    // 必須一個一個增加，因為asynchronous 會導致可能先end() 結果還沒送出檔案
-  }
-});
-
-server.listen(3000, () => {
-  console.log("正在3000運行中");
-});
+console.log(
+  cowsay.say({
+    text: "牛牛!",
+    e: "oO",
+    T: "U ",
+  })
+);
