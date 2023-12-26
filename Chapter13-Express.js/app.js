feef53f525a105e6cf11e1e6c659597416b665e8
Oni <qw28425382694@gmail.com>
2023-12-26 14:35:43 +0800

    Ch13 - section251 request物件的常用屬性，req.params可以得到router後面/:someFruit的部分，透過req.params.somFruit取出，然後app.get('*') 可以防止別人搜尋錯誤地址出現cannot get xxx、req.query 則是可以取得表單的keyvaluePair，req.query.name之類，req.body比較奇特，必須要在最前面加上app.use(express.json()) 跟express.urlencoded()、否則無法取得post轉換為req.body，得不到req.body.email 或密碼

--- Chapter13-Express.js/app.js
+++ Chapter13-Express.js/app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express(); //回傳一個物件過來
 
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // HTTP request GET  POST PUT DELETE
 
 app.get("/", (req, res) => {
@@ -11,24 +15,53 @@ app.get("/anotherPage", (req, res) => {
   res.send("這是另一個頁面");
 });
 
-app.get("/example", (req, res) => {
-  // res.send("<h1>這是一個h1標籤示範</h1>");
-  // res.send("<p>這是一個段落</p>");
-  /**           傳送網頁.html                */
-  // 下面一定要用absolutely rout
-  // res.sendFile(__dirname + "/example.html");
-  /**       物件傳送過去變成json格式              */
-  // let obj = {
-  //   title: "Web Design",
-  //   website: "Udemy",
-  // };
-  // res.json(obj);
-  /**         導向另一個url或網址  */
-  res.redirect("/newPosition");
-});
+// app.get("/example", (req, res) => {
+// res.send("<h1>這是一個h1標籤示範</h1>");
+// res.send("<p>這是一個段落</p>");
+/**           傳送網頁.html                */
+// 下面一定要用absolutely rout
+// res.sendFile(__dirname + "/example.html");
+/**       物件傳送過去變成json格式              */
+// let obj = {
+//   title: "Web Design",
+//   website: "Udemy",
+// };
+// res.json(obj);
+/**         導向另一個url或網址  */
+// res.redirect("/newPosition");
+// });
 
 app.get("/newPosition", (req, res) => {
-  console.log(res.send("這邊才是正確資源位置"));
+  res.send("這邊才是正確資源位置");
+});
+
+app.get("/fruit", (req, res) => {
+  res.send("歡迎來到水果頁面");
+});
+app.get("/fruit/:someFruit", (req, res) => {
+  res.send(req.params.someFruit);
+});
+app.get("/example", (req, res) => {
+  res.sendFile(__dirname + "/example.html");
+});
+app.get("/formhandling", (req, res) => {
+  console.log(req.query);
+  res.send(
+    "伺服器已經收到表單，你收到的資料如下" +
+      "<br>" +
+      "名稱:" +
+      req.query.name +
+      "年紀:" +
+      req.query.age
+  );
+});
+app.post("/formhandling", (req, res) => {
+  console.log(req.body);
+  res.send("帳號是" + req.body.email);
+});
+
+app.get("*", (req, res) => {
+  console.log(res.send("你所找的頁面不存在"));
 });
 
 // port callback
