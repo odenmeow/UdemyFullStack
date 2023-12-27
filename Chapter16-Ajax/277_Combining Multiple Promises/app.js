fdbdcfb1a79555d44f59711f24934edb78257f43
Oni <qw28425382694@gmail.com>
2023-12-27 15:47:38 +0800

    Ch16 section - 277 combining multiple promises 介紹 Promise.all 跟 catch 應用，然後簡單提到any，沒深入說明，GPT說any 的.then是最先完成的才會執行，catch則是全出錯才執行

---
+++ Chapter16-Ajax/277_Combining Multiple Promises/app.js
@@ -0,0 +1,19 @@
+const fetchPromise1 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+);
+const fetchPromise2 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found"
+);
+const fetchPromise3 = fetch(
+  "https://mdn.gihub.io/learning-area/javascript/oojs/json/superheroes.json"
+);
+
+Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+  .then((responses) => {
+    responses.forEach((response) => {
+      console.log(response.url, response.status);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
