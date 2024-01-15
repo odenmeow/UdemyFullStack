a3dee0cf7cb50053ff289c1de8bfadbbe2ab1f15
Oni <qw28425382694@gmail.com>
2024-01-15 17:03:02 +0800

    Project7 section 345 製作post，包含模型POST、包含取得post ejs 跟 post的文章內容、然後profile.ejs這邊也要能取得/profile/ 這個路徑下撈資料後給予的posts，用來生成卡片資訊

--- Project7_GoogleLoginWeb/routes/profile-routes.js
+++ Project7_GoogleLoginWeb/routes/profile-routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const Post = require("../models/post-model");
 const authCheck = (req, res, next) => {
   if (req.isAuthenticated()) {
     next();
@@ -6,9 +7,29 @@ const authCheck = (req, res, next) => {
     return res.redirect("/auth/login");
   }
 };
-router.get("/", authCheck, (req, res) => {
+router.get("/", authCheck, async (req, res) => {
   console.log("已進入 >> /profile");
-  return res.render("profile", { user: req.user });
+  let postFound = await Post.find({ author: req.user._id }).exec();
+
+  return res.render("profile", { user: req.user, posts: postFound });
   // deSerial那邊有解釋req.user
+});
+router.get("/post", authCheck, (req, res) => {
+  return res.render("post", { user: req.user });
+});
+router.post("/post", authCheck, async (req, res) => {
+  let { title, content } = req.body;
+  let newPost = new Post({
+    title,
+    content,
+    author: req.user._id,
+  });
+  try {
+    let savedPost = await newPost.save();
+    return res.redirect("/profile");
+  } catch (e) {
+    req.flash("error_msg", "標題跟內容都要填寫");
+    return res.redirect("/profile/post");
+  }
 });
 module.exports = router;
