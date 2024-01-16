e55085d6dbddc50cc0334da1bfc58849631d7428
Oni <qw28425382694@gmail.com>
2024-01-16 15:04:06 +0800

    Ch23 section 349 Started with React、簡單介紹與初步設定

---
+++ Chapter24-React/README.md
@@ -0,0 +1,3 @@
+透過 npx create-react-app myact
+
+安裝 React 專案 ， 名稱只能有小寫
---
+++ Chapter24-React/myact/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <h1>這是 app.js H1</h1>
+    </div>
+  );
+}
+
+export default App;
