ec0398f42708cf05a2d4dc0c90041f25d7409270
Oni <qw28425382694@gmail.com>
2024-01-16 23:31:13 +0800

    Ch23 section 354 事件處理，主要也只講這個，JSX的事件處理使用{functionName}，或者{()=>fn(param)}避免直接被調用造成App.js運行時多次直接調用運作該fn

--- Chapter24-React/myact/src/App.js
+++ Chapter24-React/myact/src/App.js
@@ -2,12 +2,16 @@ import Nav from "./nav";
 import Info from "./info";
 function App() {
   let friends = ["咪咪", "阿橘", "黑皮"];
+  const buttonHandler = (msg) => {
+    alert(msg);
+  };
   return (
     <div>
       <Nav />
       {friends.map((friend) => (
         <Info name={friend.name} age={friend.age} />
       ))}
+      <button onClick={() => buttonHandler("天氣不錯喔")}>按我</button>
     </div>
   );
 }
