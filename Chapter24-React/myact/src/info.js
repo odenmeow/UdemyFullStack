c3cfeb944e0a72dbc9f40c39c0286ed7efaac530
Oni <qw28425382694@gmail.com>
2024-01-17 00:15:49 +0800

    Ch23 section 355 State，包含Chrome,Edge可安裝React Developer Tools 從f12觀看hooks跟state長怎樣，然後例如info.js要內部export的變數應該是大寫開頭，然後快速解構物件參數，還有陣列參數，都有體現，使用了useState，屬於React.useState，可以讓onClick的時候真正參與作用，使用useState才會被Re render

--- Chapter24-React/myact/src/info.js
+++ Chapter24-React/myact/src/info.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/style.css";
 
-const info = (props) => {
-  console.log(props);
+const Info = () => {
+  let [name, setName] = useState("小明");
+  let age = 20;
+  const changeNameHandler = () => {
+    setName("小明星");
+  };
   return (
     <div className="info">
-      <h1>名稱:{props.name}</h1>
-      <h1>年紀:{props.age}</h1>
+      <h1>名稱:{name}</h1>
+      <h1>年紀:{age}</h1>
+      <button onClick={changeNameHandler}>改名按鈕</button>
     </div>
   );
 };
 
-export default info;
+export default Info;
