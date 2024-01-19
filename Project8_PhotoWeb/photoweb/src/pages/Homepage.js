e59c622a470c7fe2bbfaf82f2c7ac7a6f2dadd89
Oni <qw28425382694@gmail.com>
2024-01-20 00:14:09 +0800

    Project8 section 365 更多圖片的功能，改寫homepage就能達成，先增加div，記得className要給對，裡面是button，然後按鈕功能就要靈活搭配之前教的功能，useState、concat或用[...data,...result.data.photos]，還有用currentSearch保持載入是找圖片的search按鈕送出後的答案，避免跟input混淆(onchange)會搗亂

--- Project8_PhotoWeb/photoweb/src/pages/Homepage.js
+++ Project8_PhotoWeb/photoweb/src/pages/Homepage.js
@@ -5,8 +5,10 @@ import axios from "axios";
 const Homepage = () => {
   let [data, setData] = useState(null);
   let [input, setInput] = useState("");
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = process.env.REACT_APP_PHOTOAPIKEY;
-  const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const initialURL = `https://api.pexels.com/v1/curated?page=1&per_page=15`;
   let searchURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
   const search = async (url) => {
     let result = await axios.get(url, {
@@ -14,6 +16,24 @@ const Homepage = () => {
     });
     console.log(result);
     setData(result.data.photos);
+    setCurrentSearch(input);
+  };
+
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1); //因為clousre所以設定完，不會拿到更改後的page
+    if (currentSearch === "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${
+        page + 1
+      }`;
+    }
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
+    //setData([...data, ...result.data.photos]); 也可以喔
   };
   useEffect(() => {
     search(initialURL);
@@ -28,6 +48,9 @@ const Homepage = () => {
       />
       <div className="pictures">
         {data && data.map((d) => <Picture data={d} />)}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
       </div>
     </div>
   );
