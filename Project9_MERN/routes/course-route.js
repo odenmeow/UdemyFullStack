7e800f3bb1ef0da27d4d7cd77878d76e2986f8f0
Oni <qw28425382694@gmail.com>
2024-01-21 21:27:54 +0800

    Project9 - section 373 課程routes設定，取得所有課程、透過populate讓講師明細顯示出來、依照id取得課程、依照id編輯課程、刪除課程

--- Project9_MERN/routes/course-route.js
+++ Project9_MERN/routes/course-route.js
@@ -7,6 +7,33 @@ router.use((req, res, next) => {
   console.log("驗證身分通過");
   next();
 });
+// 取得所有課程
+router.get("/", async (req, res) => {
+  try {
+    let foundCourse = await Course.find({})
+      .populate("instructor", ["username", "email", "password"])
+      .exec();
+
+    res.send(foundCourse);
+  } catch (error) {
+    return res.status(500).send(error);
+  }
+});
+
+// 依照課程id 找課程
+router.get("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  try {
+    let foundCourse = await Course.findOne({ _id })
+      .populate("instructor", ["email", "username"])
+      .exec();
+    console.log(foundCourse);
+    return res.send(foundCourse);
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+// 新增課程
 router.post("/", async (req, res) => {
   //數據要先符合規範
   let { error } = courseValidation(req.body);
@@ -27,5 +54,53 @@ router.post("/", async (req, res) => {
   } catch (e) {
     return res.status(500).send("無法創建課程");
   }
+});
+
+// 更改課程
+router.patch("/:_id", async (req, res) => {
+  // 驗證數據是否符合規範
+  let { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  let { _id } = req.params;
+  // 先確認course存在與否
+  try {
+    let foundCourse = await Course.findOne({ _id });
+    if (!foundCourse) {
+      return res.status(400).send("查無該課程，無法進行內容更新");
+    }
+    //必須為該課程講師才能修改
+    if (foundCourse.instructor.equals(req.user._id)) {
+      let updatedCourse = await Course.findOneAndUpdate({ _id }, req.body, {
+        new: true,
+        runValidators: true,
+      }).exec();
+      return res.send({
+        message: "課程已經更新成功",
+        updatedCourse,
+      });
+    } else {
+      return res.status(403).send("課程講師才能編輯");
+    }
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+});
+
+router.delete("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  // 確認課程存在
+  try {
+    let foundCourse = await Course.findOne({ _id });
+    if (!foundCourse) {
+      return res.status(400).send("找不到課程，無法刪除");
+    }
+    // 必須是該講師才能刪
+    if (foundCourse.instructor.equals(req.user._id)) {
+      let deleteCourse = await Course.findOneAndDelete({ _id }).exec();
+      return res.send("課程已被刪除");
+    } else {
+      return res.status(403).send("只有課程講師能刪除");
+    }
+  } catch (err) {}
 });
 module.exports = router;
