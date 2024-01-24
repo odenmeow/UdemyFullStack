f79af6162e14a3ecd061d0bfce899de0d584ccc6
Oni <qw28425382694@gmail.com>
2024-01-24 21:36:29 +0800

    Ch25 section 389，介紹CSS modules跟 App Component，一個寫好單一CSS，import能套用到目標component身上，只要給出className = {nameInCss}，另一個則是app全域套用預設

--- Chapter25-Next.js/my-next/components/layout.js
+++ Chapter25-Next.js/my-next/components/layout.js
@@ -3,20 +3,27 @@ import Head from "next/head";
 const name = "Oni";
 const websiteTitle = "Next.js 網站練習";
 import Link from "next/link";
+
+import styles from "./layout.module.css";
+
 export default function Layout({ children, returnBack }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <meta name="author" content="Oni" />
       </Head>
-      <header>
+      <header className={styles.header}>
         <h1>{websiteTitle}</h1>
         <h2>創建人{name}</h2>
       </header>
       <main>{children}</main>
-      {returnBack && <Link href="/">回到首頁</Link>}
+      {returnBack && (
+        <Link href="/" className={styles.home}>
+          回到首頁
+        </Link>
+      )}
     </div>
   );
 }
